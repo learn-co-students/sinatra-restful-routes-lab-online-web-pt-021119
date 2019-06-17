c085473466a3a91b4b2e34d70f33ade4eb875c2f
Allen <allenfp@gmail.com>
2019-06-17 00:11:41 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes/new' do 
+    erb :new
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all 
+    erb :index
+  end
+
+  get '/recipes/:id' do 
+    @recipe = Recipe.find_by(:id => params[:id])
+    erb :recipe
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit    
+  end
+  
+  patch '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name, @recipe.indgredients, @recpie.cook_time = params[:name], params[:ingredients], params[:cook_time]
+    @recipe.save 
+    redirect "/recipes/#{@recipe.id}"   
+  end
+
+  post '/recipes' do  
+    @recipe = Recipe.create(params)
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  # delete '/recipes/:id/delete' do 
+  delete '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+  end
 
 end
+# https://stackoverflow.com/questions/22074545/difference-between-redirect-and-redirect-to-in-sinatra
